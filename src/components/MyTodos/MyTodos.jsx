a48633a465435dcee6edf0d9094b809e82825255
matthewkim1005 <matt_hew_kim@yahoo.com>
2024-09-03 13:13:58 -0700

    added redirect file.

---
+++ src/components/MyTodos/MyTodos.jsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+import styles from './MyTodos.module.css';
+
+const TodoList = (props) => {
+    return (
+        <main>
+            <div className={styles.center}>
+                <Link className={styles.addTodo} to="/todos/new">NEW TODO</Link>
+            </div>
+            <div className={styles.container}>
+                {props.todos.map((todo) => (
+                    <Link key={todo._id} to={`/todos/${todo._id}`}>
+                        <article>
+                            <header>
+                                <h2>{todo.task}</h2>
+                                <p>
+                                    {todo.creator.username}
+                                </p>
+                                {todo.creator._id === user._id && (
+                                    "WOW"
+                                )}
+                            </header>
+                            <p>{todo.details}</p>
+                        </article>
+                    </Link>
+                ))}
+            </div>
+        </main>
+    );
+};
+
+export default TodoList;
